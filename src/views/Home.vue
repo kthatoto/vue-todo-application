ce7d5511c666b0a6e7a29c641703152891951abe
kthatoto <kthatoto@gmail.com>
2019-05-02 17:31:31 +0900

    Todo application

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,93 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>未達成</h2>
+    <ul>
+      <li v-for="task in uncheckedTasks" :key="task.id">
+        <label>
+          <input type="checkbox" v-model="task.checked">
+          <span>{{ task.name }}</span>
+          <button @click="deleteTaskById(task.id)">削除</button>
+        </label>
+      </li>
+    </ul>
+    <div class="console">
+      <input type="text" v-model="newTaskName">
+      <button @click="addTask">追加</button>
+    </div>
+    <h2>達成済み</h2>
+    <ul>
+      <li v-for="task in checkedTasks" :key="task.id">
+        <label>
+          <input type="checkbox" v-model="task.checked">
+          <span>{{ task.name }}</span>
+          <button @click="deleteTaskById(task.id)">削除</button>
+        </label>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      tasks: [],
+      newTaskName: ''
+    }
+  },
+  computed: {
+    checkedTasks () {
+      return this.tasks.filter(t => t.checked)
+    },
+    uncheckedTasks () {
+      return this.tasks.filter(t => !t.checked)
+    }
+  },
+  created () {
+    this.tasks = JSON.parse(localStorage.getItem('tasks'))
+  },
+  methods: {
+    deleteTaskById (id) {
+      const taskIndex = this.tasks.findIndex(t => t.id === id)
+      this.tasks.splice(taskIndex, 1)
+    },
+    addTask () {
+      const newId = Math.max.apply(null, this.tasks.map(t => {
+        return t.id
+      })) + 1
+      this.tasks.push({
+        id: newId,
+        name: this.newTaskName,
+        checked: false
+      })
+    }
+  },
+  watch: {
+    tasks: {
+      handler () {
+        localStorage.setItem('tasks', JSON.stringify(this.tasks))
+      },
+      deep: true
+    }
   }
 }
 </script>
+
+<style lang="scss">
+.home {
+  li {
+    list-style: none;
+    span {
+      cursor: pointer;
+      display: inline-block;
+      width: 200px;
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  }
+  .console {
+    padding: 0 20px;
+  }
+}
+</style>
